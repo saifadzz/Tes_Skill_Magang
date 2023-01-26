c4735d57753164f1b096c6df073133e3aa1e1b0f
Saif Adzzahabi <106733480+saifadzz@users.noreply.github.com>
2023-01-26 10:08:57 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -61,6 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  var date = DateTime();
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -75,41 +76,40 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      
+body: Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/02/28/11/47/forest-653448_1280.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+        
+        Container(
+          width: 200,
+          height: 200,
+          child: Text(
+            date.hour.toString() + " : " + date.minute.toString(),
+          style: TextStyle(color: Colors.white, fontSize: 50)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        Positioned(
+          right: 40.0,
+          top: 100.0,
+          child: Text(date.hour.toString() + ':' + date.minute.toString(),
+          style: TextStyle(color: Colors.white, fontSize: 50)),
+        ),
+        Positioned(
+          right: 40.0,
+          top: 150.0,
+          child: Text("BelajarFlutter.com",
+          style: TextStyle(color: Colors.white, fontSize: 16.0)),
+        ),
+      ]  
+    ),
+      );
 }
+}  
